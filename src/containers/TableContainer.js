ad4043ccc75d51ab96822350f76fc3abb23914c6
bushig <azerot966@gmail.com>
2017-02-14 23:54:21 +0300

    Filter fix

--- src/containers/TableContainer.js
+++ src/containers/TableContainer.js
@@ -8,16 +8,10 @@ import {setActiveDeck, setPlaneRects} from '../actions/planeAction'
 
 
 const getVisibleTableList = (fullList, range, text) => { // TODO: Optimize, refactor
-    console.log('TEXT:', text);
-    if (range && range.length === 2 && text){
         console.log('Фильтруем');
         return fullList.filter(function(item) {
             return range[0] <= item.index && item.index<range[1]+1 && item.title.toLowerCase().includes(text.toLowerCase());
         })
-    } else {
-        console.log('не Фильтруем');
-        return fullList
-    }
 };
 
 const mapStateToProps = (state) => {
